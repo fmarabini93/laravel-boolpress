fa54f7e7f9744c43b5118dc35a542906410e05ae
fmarabini93 <fmarabini@icloud.com>
2021-07-26 14:35:32 +0200

    front_office

---
+++ resources/js/views/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+      <div>
+        <h1>Work in Progress</h1>
+        <h3>Site under maintenance</h3>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {}
+}
+</script>
+
+<style scoped>
+    .container {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+</style>>
